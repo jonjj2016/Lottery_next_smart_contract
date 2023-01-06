5156878278d4196d2c15a7eed18834769bd72982
jonJJ2016 <jonmartirosyan1988@gmail.com>
2023-01-06 22:52:09 +0800

    iuygiyg

---
+++ Components/Header.jsx
@@ -0,0 +1,11 @@
+import Head from 'next/head'
+
+export default function Header() {
+  return (
+    <Head>
+      <title>Smart Contract Lottery</title>
+      <meta name="description" content="Smart Lottery" />
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+  )
+}
